0acbe0782eed11af80a341e7a64d626cb10089f5
Tanizia Fagundes <tdlf@icomp.ufam.edu.br>
2021-07-07 14:07:02 -0400

    feat: initial construction menu components

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,15 @@
+import * as S from "./styled";
+
+const Menu = ({}) => {
+  return (
+    <S.Container>
+      <S.Content>
+        <ul>
+          <li>Home</li>
+          <li>Produtos</li>
+          <li>Minha Conta</li>
+        </ul>
+      </S.Content>
+    </S.Container>
+  );
+};
