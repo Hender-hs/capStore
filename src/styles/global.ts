6a4393dceefef1e7d459a5e4cbc90d9c0f3ebcad
Leonardo Lima <leoexori@hotmail.com>
2021-07-08 10:30:10 -0300

    global variables and global font added

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    html, body, figure, div, input, textarea, p, h1, h2, h3, h4, h5, h6, ul, a{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    font-family: 'Roboto Condensed', sans-serif;
+}
+:root{
+  --alt-font: 'Poppins', sans-serif; 
+  --dark-green: #0D2818;
+  --green: #14AF5A;
+  --light-green: #16DB65;
+  --lighter-green: #A9FDAC;
+  --gray: #605F5F;
+  --black: #1C1B1B;
+}
+
+`;
+
+export default GlobalStyle;
