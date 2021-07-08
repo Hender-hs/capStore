cb77d78c7f1394b0fbace8074d0e159bb87a8fba
Paulo Thor <paulothor12345@gmail.com>
2021-07-07 22:00:06 -0300

    feat: add product component

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,3 +1,6 @@
+import formatValue from "../../utils/formatValue";
+import DefaultImage from "../../assets/default.jpg";
+
 import * as S from "./styles";
 
 interface Feedback {
@@ -14,13 +17,32 @@ interface ProductProps {
   id?: number;
   url: string;
   quantity?: number;
-  sellerId: number;
+  sellerId?: number;
   feedback?: Feedback[];
   userType: string;
 }
 
-const Product = ({ name, price, quantity, userType }: ProductProps) => {
-  return <S.Container></S.Container>;
+const Product = ({ name, url, price, quantity, userType }: ProductProps) => {
+  return (
+    <S.Container>
+      <img src={url || DefaultImage} alt={name} />
+      <h3>{name}</h3>
+      {userType === "seller" ? (
+        <S.SellerPrice>
+          <label>{formatValue(price)}</label>
+          <p>Quantidade vendida: {quantity}</p>
+        </S.SellerPrice>
+      ) : (
+        <S.ClientPrice>
+          <p>
+            <label>À vista</label>
+            {formatValue(price)}
+          </p>
+          <h6>ou 12x sem juros de {formatValue(price / 12)}</h6>
+        </S.ClientPrice>
+      )}
+    </S.Container>
+  );
 };
 
 export default Product;
--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -1,3 +1,43 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 250px;
+  margin: 1rem auto;
+  background: gray;
+  padding: 0.5rem 1rem;
+  border-radius: 10px;
+
+  img {
+    width: 220px;
+    border: none;
+    padding-top: 1rem;
+  }
+
+  h3 {
+    color: #404040;
+    font-size: 20px;
+    font-weight: 600;
+  }
+`;
+
+export const SellerPrice = styled.div`
+  label {
+    color: green;
+  }
+
+  p {
+    color: #404040;
+  }
+`;
+
+export const ClientPrice = styled.div`
+  label {
+    color: green;
+    padding-right: 0.3rem;
+  }
+
+  h6 {
+    font-size: 14px;
+    margin: 1rem 0;
+  }
+`;
