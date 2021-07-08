957e8ced871b810e7aa96083a36eb80e24f59b88
Nicole-Malaquias <nicolemalaquias91@gmail.com>
2021-07-08 14:48:05 -0300

    refactor : input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from "react";
-import Routes from "./routes";
+// import React from "react";
+// import Routes from "./routes";
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <Routes />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import HomeClient from "./pages/HomeClient";
 function App() {
   return (
     <div className="App">
-      <Routes />
+      <HomeClient />
     </div>
   );
 }
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -2,8 +2,8 @@ const Input = ({ label, type, register, name, error = "", ...rest }) => {
   return (
     <>
       <label htmlFor={name}>{label}</label>
-      <input type={type} {...register(name)} {...rest} />
-      {!!error && <p>- {error}</p>}
+      <input type={type} {...rest} />
+      {!!error && <p>-{error}</p>}
     </>
   );
 };
