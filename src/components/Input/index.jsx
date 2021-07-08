642c922a8667b417f791ded94473447e321e312b
Nicole-Malaquias <nicolemalaquias91@gmail.com>
2021-07-08 13:07:33 -0300

    refactor : archive to js

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,10 @@
+const Input = ({ label, type, register, name, error = "", ...rest }) => {
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <input type={type} {...register(name)} {...rest} />
+      {!!error && <p>- {error}</p>}
+    </>
+  );
+};
+export default Input;
