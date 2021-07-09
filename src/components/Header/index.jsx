552c7a40e3e444c0b1a59377fa61ac5fe0b4bcf3
Paulo Thor <paulothor12345@gmail.com>
2021-07-09 13:24:05 -0300

    feat: add menu component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,90 @@
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+import { FiLogIn, FiShoppingCart } from "react-icons/fi";
+
+import * as S from "./styles";
+import { Badge, createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+import { useAuth } from "../../providers/Auth";
+import { useCart } from "../../providers/Cart";
+
+const innerTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2ca81c",
+    },
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    justifyContent: "space-between",
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+    fontFamily: "Nunito, sans-serif",
+    fontWeight: 600,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  cartIcon: {
+    padding: "0 10px",
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+  const classes = useStyles();
+
+  const { token } = useAuth();
+  const { cart } = useCart();
+
+  return (
+    <ThemeProvider theme={innerTheme}>
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        className={classes.appBar}
+      >
+        <Toolbar className={classes.toolbar}>
+          <S.NavLink to="/">
+            <Typography
+              variant="h6"
+              color="inherit"
+              noWrap
+              className={classes.toolbarTitle}
+            >
+              Kenzie Shop
+            </Typography>
+          </S.NavLink>
+          <nav>
+            <S.NavLink to="/cart">
+              <Badge badgeContent={cart.length} color="primary">
+                <FiShoppingCart
+                  size={25}
+                  color="green"
+                  className={classes.cartIcon}
+                />
+              </Badge>
+              <S.StyledSpan> Pedidos </S.StyledSpan>
+            </S.NavLink>
+          </nav>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const NavLink = styled(Link)`
+  margin: 10px;
+  font-size: 18px;
+
+  color: #333;
+
+  > svg {
+    transform: translateY(3.5px);
+    margin-right: 10px;
+  }
+
+  span {
+    margin-left: 5px;
+  }
+`;
+
+export const StyledSpan = styled.span`
+  padding-left: 10px;
+`;
