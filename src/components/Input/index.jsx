5eba1528095cf351e6e1a8616ac49b4419c4f131
Leonardo Lima <leoexori@hotmail.com>
2021-07-09 12:23:52 -0300

    feature/register done and input component modified

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -2,13 +2,8 @@ const Input = ({ label, type, register, name, error = "", ...rest }) => {
   return (
     <>
       <label htmlFor={name}>{label}</label>
-<<<<<<< HEAD
-      <input type={type} {...rest} />
-      {!!error && <p>-{error}</p>}
-=======
-      <input type={type}{...rest} />
+      <input type={type}{...register(name)}{...rest} />
       {!!error && <p>- {error}</p>}
->>>>>>> 0387da4881d1073020da1fc8ed4d0406749c479a
     </>
   );
 };
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -3,12 +3,16 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import{Link,Redirect,useHistory} from "react-router-dom"
 import api from "../../services/api"
+import{ toast } from 'react-toastify'
+import Container, { Select } from './styles'
+import Input from '../../components/Input'
+import Button from '../../components/Button';
 
 const Register = () => {
   const formSchema = yup.object().shape({
     name: yup.string().required("Campo obrigatório"),
     email: yup.string().email("Email inválido").required("Campo obrigatório"),
-    phone: yup.string().matches(/^[0-9]{2}-[0-9]{4}-[0-9]{4}$/, "Apenas números entre 0 e 9 são válidos").required("Campo obrigatório").min(8),
+    phone: yup.string().matches(/(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})/, "Apenas números entre 0 e 9 são válidos").required("Campo obrigatório").min(8),
     cep: yup.string().matches(/^\d{5}-\d{3}$/, "Insira seu CEP com o formato válido, ex.: 00000-111").required("Campo Obrigatório"),
     password: yup.string().matches(/^((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
     "Sua senha deve conter ao menos 8 caracteres, uma letra maíuscula, uma letra minúscula, um número e um caracter especial."
@@ -18,7 +22,7 @@ const Register = () => {
     typeUser: yup.string().required("Campo obrigatório")
   })
 
-  const {register, handleSubmit, formstate: {errors}} = useForm({
+  const {register, handleSubmit, formState: {errors}} = useForm({
     resolver: yupResolver(formSchema)
   })
   const history = useHistory()
@@ -31,15 +35,31 @@ const Register = () => {
       return history.push('/login')
     })
     .catch((err) => toast.error("Erro ao criar usuario"))
+    console.log("done")
   }
 
-  const token = localStorage.getItem("@GestãoDeHábitos:access") || false;
+  const token = localStorage.getItem("@capstore:accessToken") || false;
   if(token) {
     return <Redirect to="/dashboard"/>
   }
 
   return (
-    <div></div>
+    <Container>
+      <form onSubmit={handleSubmit(handleSubmitFunction)}>
+        <h1>Criar Conta</h1>
+        <Input label="Nome" type="text" register={register} name="name" error={errors.name?.message} />
+        <Input label="Email" type="text" register={register} name="email" error={errors.email?.message} />
+        <Input label="Telefone" type="text" register={register} name="phone" error={errors.phone?.message} placeholder="51 9999-9999" />
+        <Input label="CEP" type="text" register={register} name="cep" error={errors.cep?.message} placeholder="00000-000"/>
+        <Input label="Senha" type="password" register={register} name="password" error={errors.password?.message} />
+        <Input label="Confirme sua Senha" type="password" register={register} name="confirmPassword" error={errors.confirmPassword?.message} />
+        <Select {...register("typeUser")}>
+        <option value="seller">Vendedor</option>
+        <option value="customer">Cliente</option>
+        </Select>
+        <Button type="submit">Confirmar</Button>
+      </form>
+    </Container>
   )
 }
 
