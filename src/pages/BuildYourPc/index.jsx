975a38a93481b6aec2c1081b9facfaefdde30d75
Henderson <Henderson.f.p@hotmail.com>
2021-07-09 00:00:45 -0300

    feature: still working on build-your-pc page

--- src/pages/BuildYourPc/index.jsx
+++ src/pages/BuildYourPc/index.jsx
@@ -1,25 +1,78 @@
-import { useProducts } from "../../providers/Products";
-import * as S from "./styles"
+import { useProducts }  from "../../providers/Products";
+import defaultImg       from "../../assets/default.jpg";
+import * as S           from "./styles"
 
 const BuildYourPc = () => {
 
-  const { products } = useProducts();
+  const { filterByCategory, filteredProducts } = useProducts();
 
-  const printProdcts = ({name, image, description}, index) => (
-    <div key={index} >
+  const printProdcts = ({name, url, description}, index) => (
+    <S.EachHardware key={index} >
       <div>
         <p>{name}</p>
       </div>
       <div>
-        <img src={image} alt={name} />
+        <img src={url} alt={name} />
         <p>{description}</p>
       </div>
-    </div>
+    </S.EachHardware>
   );
 
   return (
     <S.Container>
-      {products.map(printProdcts)}
+      <S.PChardware>
+        <S.EachHardware onClick={() => filterByCategory("Placa Mãe")} >
+          <img src={defaultImg} alt="Placa Mãe" />
+          <p>Placa Mãe</p>
+        </S.EachHardware>
+        <S.EachHardware onClick={() => filterByCategory("Placa de Vídeo")} >
+          <img src={defaultImg} alt="Placa de Vídeo" />
+          <p>Placa de Vídeo</p>
+        </S.EachHardware>
+        <S.EachHardware onClick={() => filterByCategory("Processador")} >
+          <img src={defaultImg} alt="Processador"  />
+          <p>Processador</p>
+        </S.EachHardware>
+        <S.EachHardware onClick={() => filterByCategory("Fonte")} >
+          <img src={defaultImg} alt="Fonte" />
+          <p>Fonte</p>
+        </S.EachHardware>
+        <S.EachHardware onClick={() => filterByCategory("Cooler")} >
+          <img src={defaultImg} alt="Cooler" />
+          <p>Cooler</p>
+        </S.EachHardware>
+        <S.EachHardware onClick={() => filterByCategory("Armazenamento")} >
+          <img src={defaultImg} alt="Armazenamento" />
+          <p>Armazenamento</p>
+        </S.EachHardware>
+        <S.EachHardware onClick={() => filterByCategory("Memoria RAM")} >
+          <img src={defaultImg} alt="Memoria RAM" />
+          <p>Memoria RAM</p>
+        </S.EachHardware>
+        <S.EachHardware onClick={() => filterByCategory("Gabinete Gamer")} >
+          <img src={defaultImg} alt="Gabinete Gamer" />
+          <p>Gabinete</p>
+        </S.EachHardware>
+        <S.EachHardware onClick={() => filterByCategory("Teclado Gamer")} >
+          <img src={defaultImg} alt="Teclado Gamer" />
+          <p>Teclado</p>
+        </S.EachHardware>
+        <S.EachHardware onClick={() => filterByCategory("Mouse Gamer")} >
+          <img src={defaultImg} alt="Mouse Gamer" />
+          <p>Mouse</p>
+        </S.EachHardware>
+        <S.EachHardware onClick={() => filterByCategory("Mousepad Gamer")} >
+          <img src={defaultImg} alt="Mousepad Gamer" />
+          <p>Mousepad</p>
+        </S.EachHardware>
+        <S.EachHardware onClick={() => filterByCategory("Headset")} >
+          <img src={defaultImg} alt="Headset" />
+          <p>Headset</p>
+        </S.EachHardware>
+      </S.PChardware>
+      <div>
+        {filteredProducts.map(printProdcts)}
+      </div>
     </S.Container>
   );
 };
--- src/pages/BuildYourPc/styles.js
+++ src/pages/BuildYourPc/styles.js
@@ -1,7 +1,27 @@
 import styled from "styled-components";
 
 export const Container = styled.div `
-  /* width: 100vw; */
-  /* min-height: 100vh; */
-  background-color: red;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+`
+
+export const PChardware = styled.div `
+  width: 60%;
+  height: min-content;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+`
+export const EachHardware = styled.div `
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin: 15px;
+  cursor: pointer;
+
+  img {
+    width: 200px;
+  }
 `
