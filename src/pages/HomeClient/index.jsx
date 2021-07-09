2e3b17edb7232568a311f3f274161c4f188709dc
Nicole-Malaquias <nicolemalaquias91@gmail.com>
2021-07-08 21:26:01 -0300

    feat: home with corressel allrigth

--- src/pages/HomeClient/index.jsx
+++ src/pages/HomeClient/index.jsx
@@ -1,6 +1,5 @@
 import Slider from "react-animated-slider";
 import "react-animated-slider/build/horizontal.css";
-import Input from "../../components/Input";
 import { useProducts } from "../../providers/Products";
 import { useState } from "react";
 const HomeClient = () => {
@@ -13,49 +12,32 @@ const HomeClient = () => {
       <input value={inputValue} onChange={(e) => setInput(e.target.value)} />
       {inputValue === "" && (
         <>
-          {console.log("o que ta em products", products)}
           <section>
-            <h3>Teclados</h3>
+            <h3>Placa Mãe</h3>
             <Slider>
-              {products.map((item, index) => {
-                item.category === "Placa-mãe" && (
-                  <>
-                    <span>{item.name}</span>
-                    <h4>{item.price}</h4>
-                  </>
-                );
-              })}
-              <div>
-                <p>Nome do produto</p>
-                <span>Preço</span>
-              </div>
+              {products
+                .filter((item) => item.category === "Monitor Gamer")
+                .map((item, index) => (
+                  <div key={index}>
+                    <span>Nome {item.name.slice(0, 20)}</span>
+                    <p>{item.price}</p>
+                    <button>Add</button>
+                  </div>
+                ))}
             </Slider>
           </section>
           <section>
             <h3>Processador</h3>
             <Slider>
-              <div>
-                <p>Nome do produto</p>
-                <span>Preço</span>
-              </div>
-            </Slider>
-          </section>
-          <section>
-            <h3>MotherBoard</h3>
-            <Slider>
-              <div>
-                <p>Nome do produto</p>
-                <span>Preço</span>
-              </div>
-            </Slider>
-          </section>
-          <section>
-            <h3>Fontes</h3>
-            <Slider>
-              <div>
-                <p>Nome do produto</p>
-                <span>Preço</span>
-              </div>
+              {products
+                .filter((item) => item.category === "Monitor Gamer")
+                .map((item, index) => (
+                  <div key={index}>
+                    <span>Nome {item.name.slice(0, 20)}</span>
+                    <p>{item.price}</p>
+                    <button>Add</button>
+                  </div>
+                ))}
             </Slider>
           </section>
         </>
@@ -65,10 +47,14 @@ const HomeClient = () => {
           <section>
             <h3>Produto encontrado</h3>
             <Slider>
-              <div>
-                <p>Nome do produto</p>
-                <span>Preço</span>
-              </div>
+              {products
+                .filter((item) => item.category === "Monitor Gamer")
+                .map((item, index) => (
+                  <div key={index}>
+                    <span>Nome {item.name.slice(0, 20)}</span>
+                    <p>{item.price}</p>
+                  </div>
+                ))}
             </Slider>
           </section>
           <div>Aqui fica o footer</div>
