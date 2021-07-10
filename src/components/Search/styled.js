57ca0438f3c9114fa2ca2847009c290a31e5350d
Tanizia Fagundes <tdlf@icomp.ufam.edu.br>
2021-07-10 00:49:58 -0400

    styled: styled the search component

--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
-export const Input = styled.input``;
-export const Button = styled.button``;
+export const Container = styled.div`
+  display: flex;
+  border: 1px solid;
+  padding: 0.5rem 1rem;
+  border-radius: 2rem;
+`;
+
+export const Input = styled.input`
+  border-style: none;
+  outline: none;
+`;
+export const Button = styled.button`
+  background: none;
+  border-style: none;
+`;
