9f3aba17ade20b0d23ae3820e4619c51cd2cfea1
Paulo Thor <paulothor12345@gmail.com>
2021-07-12 20:55:05 -0300

    feat: add notification icon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,90 +1,26 @@
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-
-import { FiLogIn, FiShoppingCart } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoIosNotifications } from "react-icons/io";
 
 import * as S from "./styles";
-import { Badge, createMuiTheme, ThemeProvider } from "@material-ui/core";
 
 import { useAuth } from "../../providers/Auth";
 import { useCart } from "../../providers/Cart";
 
-const innerTheme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#605F5F",
-    },
-  },
-});
-
-const useStyles = makeStyles((theme) => ({
-  "@global": {
-    ul: {
-      margin: 0,
-      padding: 0,
-      listStyle: "none",
-    },
-  },
-  appBar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-  toolbar: {
-    justifyContent: "space-between",
-  },
-  toolbarTitle: {
-    flexGrow: 1,
-    fontFamily: "Nunito, sans-serif",
-    fontWeight: 600,
-  },
-  link: {
-    margin: theme.spacing(1, 1.5),
-  },
-  cartIcon: {
-    padding: "0 10px",
-  },
-}));
-
 export default function PrimarySearchAppBar() {
-  const classes = useStyles();
-
-  const { token } = useAuth();
+  const { user } = useAuth();
   const { cart } = useCart();
 
   return (
-    <ThemeProvider theme={innerTheme}>
-      <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        className={classes.appBar}
-      >
-        <Toolbar className={classes.toolbar}>
-          <S.NavLink to="/">
-            <Typography
-              variant="h6"
-              color="inherit"
-              noWrap
-              className={classes.toolbarTitle}
-            >
-              Menu
-            </Typography>
-          </S.NavLink>
-          <nav>
-            <S.NavLink to="/cart">
-              <Badge badgeContent={cart.length} color="primary">
-                <FiShoppingCart
-                  size={25}
-                  color="black"
-                  className={classes.cartIcon}
-                />
-              </Badge>
-              <S.StyledSpan> Pedidos </S.StyledSpan>
-            </S.NavLink>
-          </nav>
-        </Toolbar>
-      </AppBar>
-    </ThemeProvider>
+    <S.Header>
+      <GiHamburgerMenu size={30} />
+      <S.NavLink to="/cart">
+        <S.Items>{cart.length}</S.Items>
+        {user?.type === "seller" ? (
+          <IoIosNotifications size={30} />
+        ) : (
+          <S.CartIcon />
+        )}
+      </S.NavLink>
+    </S.Header>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,9 +1,12 @@
+import { FiShoppingCart } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const NavLink = styled(Link)`
-  margin: 10px;
   font-size: 18px;
+  display: flex;
+  justify-self: flex-end;
+  margin-left: auto;
 
   color: #333;
 
@@ -20,4 +23,30 @@ export const NavLink = styled(Link)`
 export const StyledSpan = styled.span`
   padding-left: 10px;
   font-size: 20px;
+`;
+
+export const Header = styled.div`
+  position: fixed;
+  margin-bottom: 70px;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Items = styled.div`
+  border-radius: 100%;
+  padding: 4px 8px;
+  background-color: gray;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+`;
+
+export const CartIcon = styled(FiShoppingCart)`
+  color: var(--dark);
+  width: 30px;
+  height: 30px;
 `;
