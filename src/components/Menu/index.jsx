f92acddd75cedfc4783e138128817f3e01ac9480
Tanizia Fagundes <tdlf@icomp.ufam.edu.br>
2021-07-12 10:16:20 -0400

    feat: menu desktop

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,15 +1,31 @@
 import * as S from "./styled";
-
-const Menu = ({}) => {
+import { Link } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useCart } from "../../providers/Cart";
+const Menu = () => {
+  const { cart } = useCart();
   return (
     <S.Container>
       <S.Content>
         <ul>
-          <li>Home</li>
-          <li>Produtos</li>
-          <li>Minha Conta</li>
+          <li>
+            <Link to="/HomeClient">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Produtos</Link>
+          </li>
+          <li>
+            <Link to="/">Minha Conta</Link>
+          </li>
         </ul>
+      </S.Content>
+      <S.Content>
+        <p>Inserir aqui component search</p>
+        <S.Cont>{cart.length}</S.Cont>
+        <AiOutlineShoppingCart size={30} />
       </S.Content>
     </S.Container>
   );
 };
+
+export default Menu;
