3b6b0321777162b0b183d92238a1008e99e9ac2d
Lucira Silva <luciracs0@gmail.com>
2021-07-12 17:25:11 -0300

    style: add basic colors and alignment

---
+++ src/components/ProductFeedback/styled.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: var(--lighter-green);
+  padding: 1rem;
+
+  > :first-child {
+    display: flex;
+    gap: 1rem;
+  }
+  @media only screen and (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    justify-items: center;
+    background-color: #fff;
+
+    p {
+      font-size: 18px;
+    }
+    > :first-child {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 80%;
+    }
+  }
+`;
+
+export const Likes = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
