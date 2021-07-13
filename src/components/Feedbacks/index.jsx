830b843795be9b33495c217e64a881e75d3d6af5
Lucira Silva <luciracs0@gmail.com>
2021-07-13 18:25:34 -0300

    faet:  add react star lib and logic to evaluation

--- src/components/Feedbacks/index.jsx
+++ src/components/Feedbacks/index.jsx
@@ -1,15 +1,18 @@
 import ProductFeedback from "../ProductFeedback";
 import Button from "../Button";
 import * as S from "./styled";
-
+import TransferModal from "../TransferModal";
+import { useState } from "react";
 const Feedbacks = ({ evaluation, userType }) => {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <S.Container>
       <div>
         <h3>Avaliações</h3>
+        <TransferModal open={openModal} />
         {userType === "client" && (
           <Button
-            handleClick={() => console.log("abre modal")}
+            handleClick={() => setOpenModal(!openModal)}
             color="black"
             width="100px"
           >
--- src/components/TransferModal/ModalEvaluation.jsx
+++ src/components/TransferModal/ModalEvaluation.jsx
@@ -1,10 +1,90 @@
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+import { AiFillStar as FullStar } from "react-icons/ai";
+import { AiOutlineStar as EmptyStar } from "react-icons/ai";
+import { BsStarHalf as HalfStar } from "react-icons/bs";
+import ReactStars from "react-rating-stars-component";
+import { useState } from "react";
+import * as S from "./styles";
+import api from "../../services/api";
+import Button from "../Button";
+import jwt_decode from "jwt-decode";
+import { useEffect } from "react";
+import { useAuth } from "../../providers/Auth";
+
 const ModalEvaluation = () => {
+  const productId = Number(localStorage.getItem("@capstone:product_Id") || "");
 
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+  const [user, setUser] = useState("");
+  const { token } = useAuth();
+  const [feedbacks, setFeedbacks] = useState([]);
+
+  const getUserName = () => {
+    const decoded = jwt_decode(token);
+    api
+      .get(`/users/${decoded.sub}`)
+      .then((response) => setUser(response.data.name));
+  };
+
+  const getFeedbacks = () => {
+    api
+      .get(`products/${productId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setFeedbacks(response.data.feedback);
+      });
+  };
+
+  const handleEvaluation = () => {
+    setFeedbacks([...feedbacks, { user, rating, comment, likes: 0 }]);
+    sendEvaluation();
+  };
+
+  const sendEvaluation = () => {
+    if (rating > 0) {
+      api.patch(`products/${productId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: feedbacks,
+      });
+    }
+  };
+
+  const onStarClick = (newValue) => {
+    setRating(newValue);
+  };
+  useEffect(() => {
+    getUserName();
+    getFeedbacks();
+  }, []);
   return (
-  <div>
+    <S.ModalContent>
+      <S.ModalHeader>
+        <label>Nota</label>
+        <ReactStars
+          count={5}
+          onChange={onStarClick}
+          size={24}
+          isHalf={true}
+          emptyIcon={<EmptyStar />}
+          halfIcon={<HalfStar />}
+          fullIcon={<FullStar />}
+          activeColor="#ffd700"
+        />
+        
+        <label htmlFor="commentary">Comentário</label>
+        <textarea
+          id="commentary"
+          onChange={(e) => setComment(e.target.value)}
+        />
+      </S.ModalHeader>
 
-  </div>);
+      <Button handleClick={handleEvaluation}>Enviar</Button>
+    </S.ModalContent>
+  );
 };
+export default ModalEvaluation;
