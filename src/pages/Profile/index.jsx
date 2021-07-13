fbb4b220a0acd9e6133a3a936f65596d51dcb6f3
Henderson <Henderson.f.p@hotmail.com>
2021-07-13 15:31:04 -0300

    refact: the code was refactored

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,23 +1,23 @@
-import * as S                   from "./styles";
-import { FaUserCircle }         from "react-icons/fa";
-import { IoWalletOutline }      from "react-icons/io5";
-import ProfileData              from "../../components/ProfileData";
-import api                      from "../../services/api";
-import jwtDecode                from "jwt-decode";
-import { useEffect, useState }  from "react";
-import formatValue              from "../../utils/formatValue";
-import { useHistory }           from "react-router-dom";
-import Header                   from "../../components/Header";
+import * as S                       from "./styles";
+import { FaUserCircle as UserIcon } from "react-icons/fa";
+import { IoWalletOutline as WalletIcon }          from "react-icons/io5";
+import ProfileData                  from "../../components/ProfileData";
+import api                          from "../../services/api";
+import jwtDecode                    from "jwt-decode";
+import { useEffect, useState }      from "react";
+import formatValue                  from "../../utils/formatValue";
+import { useHistory }               from "react-router-dom";
+import Header                       from "../../components/Header";
 
 const Profile = () => {
 
-  const token = localStorage.getItem("@capstore:accessToken");
+  const token = localStorage.getItem("@capstore:token");
 
   const history = useHistory();
 
   if (!token) history.push("/")
 
-  const [ userData, setUserData ] = useState();
+  const [ userData, setUserData ] = useState({});
 
   const getUserDatas = () => {
     const userId = jwtDecode(token).sub;
@@ -39,14 +39,14 @@ const Profile = () => {
           <S.Body>
             <S.Div>
               <S.LeftDiv>
-                <FaUserCircle style={{fontSize: "200px"}} />
+                <UserIcon size="200px" />
                 <h3>{userData.name}</h3>
                 <h3>{userData.location}</h3>
                 { 
                   userData.type !== "client"
                   && 
                   <S.Wallet>
-                    <IoWalletOutline style={{fontSize: "100px"}} />
+                    <WalletIcon size="100px" />
                     <h4>{formatValue(userData.cash)}</h4>
                   </S.Wallet>
                 }
