7b60b0995717a7f7a9c1f34febae6db6a7c103f9
Tanizia Fagundes <tdlf@icomp.ufam.edu.br>
2021-07-14 12:47:05 -0400

    feat: add hsitory in logout

--- src/components/MenuMobile/index.jsx
+++ src/components/MenuMobile/index.jsx
@@ -3,12 +3,12 @@ import { IoMenuOutline, IoPersonCircleOutline } from "react-icons/io5";
 import { AiOutlineHome } from "react-icons/ai";
 import { GrLogout } from "react-icons/gr";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../../providers/Auth";
 const MenuMobile = () => {
   const [modal, setModal] = useState();
   const { logout } = useAuth();
-
+  const history = useHistory();
   const handleclick = () => {
     modal ? setModal(false) : setModal(true);
   };
@@ -21,22 +21,21 @@ const MenuMobile = () => {
         </button>
         {modal ? (
           <S.Modal>
-            <Link to="/">
+            <Link to="/Dashboard">
               <IoPersonCircleOutline />
               <p>Minha Conta</p>
             </Link>
-            <Link to="/Dashboard">
+            <Link to="/Home">
               <AiOutlineHome />
             </Link>
             <S.Itens>
-              <p>ao invéns de links aqui vai o component da nicole</p>
               <Link>Computadores</Link>
               <Link>Monitores</Link>
               <Link>Hardware</Link>
               <Link>Perifericos</Link>
               <Link>Monte seu pc</Link>
             </S.Itens>
-            <button onClick={logout}>
+            <button onClick={() => logout(history)}>
               <GrLogout />
             </button>
           </S.Modal>
