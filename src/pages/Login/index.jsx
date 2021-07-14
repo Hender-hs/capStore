afd6c0e241a67b814f31796b565ebee0479f22e1
Leonardo Lima <leoexori@hotmail.com>
2021-07-14 11:56:25 -0300

    styled/login mobile done

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,17 +1,31 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as yup from "yup";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../../providers/Auth";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
-import { ContainerForm, Container } from "./styles";
+import { ContainerForm, Container, Wrapper, RegisterButton, InputDiv, Title } from "./styles";
+import Lottie from 'react-lottie';
+import buying from '../../assets/lotties/buying.json'
+
 
 function Login() {
   const { signIn } = useAuth();
 
   const [error, setError] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  const breakpoint = 768;
+
+  const buyerOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: buying,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
 
   const schema = yup.object().shape({
     email: yup.string().email().required("Campo obrigatório"),
@@ -35,37 +49,71 @@ function Login() {
     signIn(data, setError, history);
   };
 
-  return (
-    <>
-    <Container>
+  useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+     window.addEventListener("resize", handleResizeWindow);
+     return () => {
+       window.removeEventListener("resize", handleResizeWindow);
+     };
+   }, []);
 
-    </Container>
-    <ContainerForm>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <Input
-            label="Email"
-            type="text"
-            register={register}
-            name="email"
-            error={errors.email?.message}
-          />
-        </div>
-        <div>
-          <Input
-            label="Senha"
-            type="password"
-            register={register}
-            name="password"
-            error={errors.password?.message}
-          />
-        </div>
-        <Button type="submit">Enviar</Button>
-      </form>
-      {error && <span> Usuário ou senha incorretas! </span>}
-    </ContainerForm>
-    </>
-  );
+  const registerFunc = () => {
+    return history.push("/register")
+  }
+
+  if (width < breakpoint) {
+    return (
+      <Wrapper>
+      <Container>
+        <Lottie 
+          options={buyerOptions}
+          width={300}
+          height={250}
+        />
+      </Container>
+      <ContainerForm>
+        <form 
+          onSubmit={handleSubmit(onSubmit)}
+          style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around"}}>
+            <Title>Entrar</Title>
+          <InputDiv>
+            <Input
+              label="Email"
+              type="text"
+              register={register}
+              name="email"
+              error={errors.email?.message}
+              placeholder="joao@mail.com"
+              style={{marginBottom: 5}}
+            />
+          </InputDiv>
+          <InputDiv>
+            <Input
+              label="Senha"
+              type="password"
+              register={register}
+              name="password"
+              error={errors.password?.message}
+              placeholder="Senha"
+              style={{marginBottom: 5}}
+            />
+          </InputDiv>
+          <Button 
+            style={{marginTop: 25, marginBottom: 25, borderRadius: 10}}
+            type="submit"
+            width="60%">Enviar</Button>
+        <p>Não possuo conta ainda. <RegisterButton onClick={registerFunc}>Registrar</RegisterButton></p>
+        </form>
+        {error && <span> Usuário ou senha incorretas! </span>}
+      </ContainerForm>
+      </Wrapper>
+    );
+  } else if (width > breakpoint){
+    return (
+      <>
+      </>
+    )
+  }
 }
 
 export default Login;
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -3,11 +3,38 @@ import styled from "styled-components";
 export const ContainerForm = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  border-radius: 20px 20px 0 0;
+  background: linear-gradient(var(--black), var(--gray));
+  height: 70%;
 `;
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  height: 30%;
+`;
+
+export const Wrapper = styled.div`
+  height: 100vh;
+`;
+
+export const RegisterButton = styled.button`
+  background-color: transparent;
+  color: lightblue;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+  font-family: 'Roboto Condensed', sans-serif;
+`;
+
+export const InputDiv = styled.div`
+  height: 110px;
+`;
+
+export const Title = styled.h3`
+  color: #fff;
+  margin: 10px;
+  font-size: 1.7rem;
 `;
