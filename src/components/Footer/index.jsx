bb39a1833ff048a27db18c562e3f522128c1c418
Lucira Silva <luciracs0@gmail.com>
2021-07-14 18:06:14 -0300

    style: add media query to product component, stylized footer and specific page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,21 +1,24 @@
 import {RiVisaFill as Visa} from "react-icons/ri";
 import { FaCcMastercard as Mastercard } from "react-icons/fa";
 import { BsCreditCard as CreditCard } from "react-icons/bs";
-
+import * as S from "./styled";
 const Footer = () => {
 
     return (
-      <div>
-        <p>
-          <a href="/">Quem somos</a>
-        </p>
-        <p>Formas de pagamento</p>
+      <S.Container>
+        <div>
+          <p>
+            <a href="/aboutUs">Quem somos</a>
+          </p>
+          <p>Formas de pagamento</p>
+        </div>
+
         <div>
           <Visa size={50} />
           <Mastercard size={50} />
           <CreditCard size={50} />
         </div>
-      </div>
+      </S.Container>
     );
 }
 export default Footer;
