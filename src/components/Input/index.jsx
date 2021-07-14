807a80007b5d128aa2b48fe1580336f194e42fb0
Tanizia Fagundes <tdlf@icomp.ufam.edu.br>
2021-07-14 11:19:27 -0400

    feat: resolved conflict in Input index

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -4,15 +4,9 @@ const Input = ({ label, type, register, name, error = "", ...rest }) => {
   return (
     <S.Container>
       <label htmlFor={name}>{label}</label>
-<<<<<<< HEAD
-      <input type={type} {...rest} />
-      {!!error && <p>-{error}</p>}
-    </>
-=======
       <input type={type} {...register(name)} {...rest} />
       {!!error && <p>- {error}</p>}
     </S.Container>
->>>>>>> e2b41346aaa47d7262ebea74145fe132f2860de4
   );
 };
 export default Input;
