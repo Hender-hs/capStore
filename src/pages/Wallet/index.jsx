177cdb145eb154f983e6da1681ca967f16bb3ec6
Paulo Thor <paulothor12345@gmail.com>
2021-07-15 14:04:21 -0300

    fix: adjust wallet base cost

--- src/pages/Wallet/index.jsx
+++ src/pages/Wallet/index.jsx
@@ -1,6 +1,6 @@
 import Header from "../../components/Header";
 import * as S from "./styles";
-import { IoWalletOutline as WalletIcon }          from "react-icons/io5";
+import { IoWalletOutline as WalletIcon } from "react-icons/io5";
 import Button from "../../components/Button";
 import { useHistory } from "react-router-dom";
 import { useState } from "react";
@@ -11,7 +11,6 @@ import formatValue from "../../utils/formatValue";
 import Input from "../../components/Input";
 
 const Wallet = () => {
-
   const history = useHistory();
 
   const [userData, setUserData] = useState();
@@ -21,7 +20,6 @@ const Wallet = () => {
   if (!token) history.push("/");
 
   const getUser = async () => {
-
     const tokenDecoded = jwtDecode(token).sub;
 
     const responseRequest = await api.get(`/users/${tokenDecoded}`);
@@ -29,7 +27,7 @@ const Wallet = () => {
     setUserData(responseRequest.data);
   };
 
-  useEffect( () => {
+  useEffect(() => {
     !userData && getUser();
   });
 
@@ -39,9 +37,17 @@ const Wallet = () => {
       <S.Body>
         <S.WalletDiv>
           <WalletIcon size="200" />
-          <S.H3>{formatValue(userData?.cash)}</S.H3>
+          <S.H3>{formatValue(userData?.cash || 0)}</S.H3>
           <S.DivButton>
-            <Button onClick={() => history.push("/withdrawMoney")} color="black" width="95%" height="70%" style={{fontSize: "17px", color: "white", cursor: "pointer"}} >Retirar Dinheiro</Button>
+            <Button
+              onClick={() => history.push("/withdrawMoney")}
+              color="black"
+              width="95%"
+              height="70%"
+              style={{ fontSize: "17px", color: "white", cursor: "pointer" }}
+            >
+              Retirar Dinheiro
+            </Button>
           </S.DivButton>
         </S.WalletDiv>
       </S.Body>
