bf5a585466972cc5cab0ecf007cb9a0406095443
Paulo Thor <paulothor12345@gmail.com>
2021-07-15 17:43:40 -0300

    fix: debug wallet

--- src/pages/Wallet/index.jsx
+++ src/pages/Wallet/index.jsx
@@ -3,33 +3,21 @@ import * as S from "./styles";
 import { IoWalletOutline as WalletIcon } from "react-icons/io5";
 import Button from "../../components/Button";
 import { useHistory } from "react-router-dom";
-import { useState } from "react";
-import api from "../../services/api";
-import jwtDecode from "jwt-decode";
 import { useEffect } from "react";
 import formatValue from "../../utils/formatValue";
-import Input from "../../components/Input";
+import { useAuth } from "../../providers/Auth";
 
 const Wallet = () => {
+  const { token, user, getUserInfo } = useAuth();
+
   const history = useHistory();
 
-  const [userData, setUserData] = useState();
-
-  const token = localStorage.getItem("@capstore:token");
+  useEffect(() => {
+    getUserInfo();
+    // eslint-disable-next-line
+  }, []);
 
   if (!token) history.push("/");
-
-  const getUser = async () => {
-    const tokenDecoded = jwtDecode(token).sub;
-
-    const responseRequest = await api.get(`/users/${tokenDecoded}`);
-
-    setUserData(responseRequest.data);
-  };
-
-  useEffect(() => {
-    !userData && getUser();
-  });
 
   return (
     <S.Container>
@@ -37,7 +25,7 @@ const Wallet = () => {
       <S.Body>
         <S.WalletDiv>
           <WalletIcon size="200" />
-          <S.H3>{formatValue(userData?.cash || 0)}</S.H3>
+          <S.H3>{formatValue(user?.cash || 0)}</S.H3>
           <S.DivButton>
             <Button
               onClick={() => history.push("/withdrawMoney")}
