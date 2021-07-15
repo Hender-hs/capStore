bd022d177114483fd0b2cc501c0a3cceacca46c7
Henderson <Henderson.f.p@hotmail.com>
2021-07-15 18:43:37 -0300

    feature: working on page

---
+++ src/pages/allProducts/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import * as S from "./styles";
+
+const AllProducts = () => {
+
+  return (
+    <S.Container>
+      <Header color="black" />
+    </S.Container>
+  );
+};
+
+export default AllProducts;
