30147e299e34420990a25ac234a8fadce9a64737
Henderson <Henderson.f.p@hotmail.com>
2021-07-14 22:49:49 -0300

    feature/style: was created a menu toggle, and made some modifications on Provider/Auth to make a correct navagations and directions on web site

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,25 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoIosNotifications } from "react-icons/io";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-
+import { useState } from "react";
+import { FaUserCircle as UserIcon }       from "react-icons/fa";
+import { useHistory } from "react-router";
 import * as S from "./styles";
 
 import { useAuth } from "../../providers/Auth";
 import { useCart } from "../../providers/Cart";
+import { Link } from "react-router-dom";
 
 const Header = ({ color = "transparent" }) => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
   const { cart } = useCart();
-
+  const [openToggleMenu, setOpenToggleMenu] = useState(false);
+  const history = useHistory();
   return (
     <>
       {color === "transparent" && (
         <S.Header color={color}>
-          <GiHamburgerMenu size={30} />
+          <GiHamburgerMenu size={30} style={{cursor: "pointer"}} onClick={() => setOpenToggleMenu(!openToggleMenu)} />
           <S.NavLink to="/cart">
             <S.Items>{cart.length}</S.Items>
             {user?.type === "seller" ? (
@@ -28,7 +32,7 @@ const Header = ({ color = "transparent" }) => {
       )}
       {color !== "transparent" && (
         <S.Header color={color}>
-          <GiHamburgerMenu size={30} color="white" />
+          <GiHamburgerMenu size={30} color="white" style={{cursor: "pointer"}} onClick={() => setOpenToggleMenu(!openToggleMenu)} />
           <S.NavLink to="/cart">
             <S.Items>{cart.length}</S.Items>
             {user?.type === "seller" ? (
@@ -39,6 +43,24 @@ const Header = ({ color = "transparent" }) => {
           </S.NavLink>
         </S.Header>
       )}
+      <S.BackgroundModal openToggleMenu={openToggleMenu} >
+        <S.ToggleMenu openToggleMenu={openToggleMenu} >
+          <div className="first-child" >
+            <GiHamburgerMenu size={30} style={{cursor: "pointer"}} onClick={() => setOpenToggleMenu(!openToggleMenu)} />
+          </div>
+          <div className="second-child" >
+            <UserIcon size="100" style={{visibility: openToggleMenu ? "visible" : "hidden"}} />
+          </div>
+          <div className="third-child" >
+            <S.ToggleMenuLink to="home" >Home</S.ToggleMenuLink>
+            <S.ToggleMenuLink to="build-your-pc" >Monte Seu PC</S.ToggleMenuLink>
+            <S.ToggleMenuLink to="profile" >Meu Perfil</S.ToggleMenuLink>
+            <S.ToggleMenuLink to="wallet" >Carteira</S.ToggleMenuLink>
+            <S.ToggleMenuLink to="aboutUs" >Sobre Nós</S.ToggleMenuLink>
+            <S.ToggleMenuLink onClick={ () => logout(history) } >Sair</S.ToggleMenuLink>
+          </div>
+        </S.ToggleMenu>
+      </S.BackgroundModal>
     </>
   );
 };
