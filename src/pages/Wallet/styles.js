153978fae50f28a710a93cfbe8171f403836be64
Paulo Thor <paulothor12345@gmail.com>
2021-07-15 19:01:47 -0300

    style: change global background

--- src/pages/Wallet/styles.js
+++ src/pages/Wallet/styles.js
@@ -1,39 +1,40 @@
 import styled from "styled-components";
 
-export const Container = styled.div `
+export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
   flex-direction: column;
-`
+`;
 
-export const Body = styled.div `
+export const Body = styled.div`
   margin-top: 75px;
   width: 100%;
   height: 95%;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
-export const WalletDiv = styled.div `
+export const WalletDiv = styled.div`
   width: 50%;
   height: 65%;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-`
+`;
 
-export const DivButton = styled.div `
+export const DivButton = styled.div`
   width: 40%;
   min-width: 180px;
   height: 15%;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
-export const H3 = styled.h3 `
+export const H3 = styled.h3`
   font-size: 45px;
-`
+  color: white;
+`;
