65714accb64f5fb9d474a5d29d520f6a43815427
Paulo Thor <paulothor12345@gmail.com>
2021-07-16 00:26:10 -0300

    feat: add multi carousel lib

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -16,6 +16,28 @@ import Button from "../../components/Button";
 import { useAuth } from "../../providers/Auth";
 import formatValue from "../../utils/formatValue";
 import animationPc from "../../assets/lotties/pc.json";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 const Home = () => {
   const [inputValue, setInput] = useState("");
@@ -68,145 +90,155 @@ const Home = () => {
   }, []);
 
   return (
-    <S.Container>
-      <Header />
-      <div style={{ marginTop: "105px" }} />
-      <img src={GabineteHome} alt="gabinete" />
-      {/* <Lottie options={DefaultOptions} width={400} height={400} /> */}
-      <div className="search">
-        <input
-          placeholder="Pesquisar categoria"
-          onChange={(e) => setInput(e.target.value)}
-        />
-      </div>
-      {/* <Lottie width="30%" height="50%" options={LaptopOptions} /> */}
-      {type !== "seller" && (
-        <>
-          {inputValue === "" && (
-            <>
-              <h1>Placa mãe</h1>
-              <Slider autoFocus="true">
-                {products
-                  .filter((item) => item.category === "Placa-mãe")
-                  .map((item) => (
-                    <S.SliderChild>
-                      <S.Card
-                        onClick={() => redirectToSpecificProductPage(item)}
-                      >
-                        <img src={item.url} alt="img de uma peça" />
-                        <span>{item.name.slice(0, 30)}</span>
-                        {/* <p>R${item.price}</p> */}
-                      </S.Card>
-                    </S.SliderChild>
-                  ))}
-              </Slider>
-
+    <>
+      <S.Container>
+        <Header />
+        <div style={{ marginTop: "105px" }} />
+        <img src={GabineteHome} alt="gabinete" />
+        <div className="search">
+          <input
+            placeholder="Pesquisar categoria"
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+        {type !== "seller" && (
+          <>
+            {inputValue === "" && (
               <>
-                <h1 style={{ color: "rgb(255, 255, 255, 0.5)" }}>
-                  Processadores
-                </h1>
-                <Slider>
+                <h1>Placa mãe</h1>
+                <Slider autofocus={true}>
                   {products
-                    .filter((item) => item.category === "Processador")
+                    .filter((item) => item.category === "Placa-mãe")
                     .map((item) => (
                       <S.SliderChild>
                         <S.Card
                           onClick={() => redirectToSpecificProductPage(item)}
                         >
-                          <img src={item.url} alt="Processador" />
+                          <img src={item.url} alt="img de uma peça" />
                           <span>{item.name.slice(0, 30)}</span>
-                          {/* <p>R${item.price}</p> */}
                         </S.Card>
                       </S.SliderChild>
                     ))}
                 </Slider>
-              </>
 
+                <>
+                  <h1 style={{ color: "rgb(255, 255, 255, 0.5)" }}>
+                    Processadores
+                  </h1>
+                  <Slider>
+                    {products
+                      .filter((item) => item.category === "Processador")
+                      .map((item) => (
+                        <S.SliderChild>
+                          <S.Card
+                            onClick={() => redirectToSpecificProductPage(item)}
+                          >
+                            <img src={item.url} alt="Processador" />
+                            <span>{item.name.slice(0, 30)}</span>
+                          </S.Card>
+                        </S.SliderChild>
+                      ))}
+                  </Slider>
+                </>
+
+                <>
+                  <h1 style={{ color: "white" }}>Monitores Gamer</h1>
+                  <Slider>
+                    {products
+                      .filter((item) => item.category === "Monitor Gamer")
+                      .map((item) => (
+                        <S.SliderChild>
+                          <S.Card
+                            onClick={() => redirectToSpecificProductPage(item)}
+                          >
+                            <img src={item.url} alt={item.name} />
+                            <span>{item.name.slice(0, 30)}</span>
+                          </S.Card>
+                        </S.SliderChild>
+                      ))}
+                  </Slider>
+                </>
+              </>
+            )}
+            {inputValue !== "" && (
               <>
-                <h1 style={{ color: "white" }}>Monitores Gamer</h1>
                 <Slider>
                   {products
-                    .filter((item) => item.category === "Monitor Gamer")
+                    .filter(
+                      (item) =>
+                        item.category.toLowerCase() === inputValue.toLowerCase()
+                    )
                     .map((item) => (
                       <S.SliderChild>
-                        <S.Card
-                          onClick={() => redirectToSpecificProductPage(item)}
-                        >
-                          <img src={item.url} alt={item.name} />
+                        <S.Card>
                           <span>{item.name.slice(0, 30)}</span>
+                          <img
+                            src={item.url}
+                            alt={item.name}
+                            onClick={() => redirectToSpecificProductPage(item)}
+                          />
+                          <Button
+                            onClick={() => addToCart(item)}
+                            className="client"
+                            width="200px"
+                            style={{ margin: "10px 0", color: "darkgreen" }}
+                          >
+                            Comprar
+                          </Button>
                         </S.Card>
                       </S.SliderChild>
                     ))}
                 </Slider>
               </>
-            </>
-          )}
-          {inputValue !== "" && (
-            <>
+            )}
+          </>
+        )}
+        {type === "seller" && (
+          <>
+            <h1 className="seller-title">Seus produtos</h1>
+            {
               <Slider>
-                {products
-                  .filter(
-                    (item) =>
-                      item.category.toLowerCase() === inputValue.toLowerCase()
-                  )
-                  .map((item) => (
-                    <S.SliderChild>
-                      <S.Card>
-                        <span>{item.name.slice(0, 30)}</span>
-                        <img
-                          src={item.url}
-                          alt={item.name}
-                          onClick={() => redirectToSpecificProductPage(item)}
-                        />
-                        {/* <p>R${item.price}</p> */}
-                        <Button
-                          onClick={() => addToCart(item)}
-                          className="client"
-                          width="200px"
-                          style={{ margin: "10px 0", color: "darkgreen" }}
-                        >
-                          Comprar
-                        </Button>
-                      </S.Card>
-                    </S.SliderChild>
-                  ))}
+                {user.products?.map((item) => (
+                  <S.SliderChild>
+                    <S.Card onClick={() => redirectToSpecificProductPage(item)}>
+                      <img src={item.url} alt={item.name} />
+                      <span>{item.name.slice(0, 30)}</span>
+                    </S.Card>
+                  </S.SliderChild>
+                ))}
               </Slider>
-            </>
-          )}
-        </>
-      )}
-      {type === "seller" && (
-        <>
-          <h1 className="seller-title">Seus produtos</h1>
-          {
-            <Slider>
-              {user.products?.map((item) => (
+            }
+            <Button
+              width="300px"
+              style={{
+                fontSize: "1.2rem",
+                borderRadius: "10px",
+                color: "white",
+                height: "50px",
+                marginBottom: "20px",
+              }}
+              onClick={getToRegisterProducts}
+            >
+              Anunciar novo produto
+            </Button>
+          </>
+        )}
+        <div>
+          <Carousel responsive={responsive}>
+            {products
+              .filter((item) => item.category === "Processador")
+              .map((item) => (
                 <S.SliderChild>
                   <S.Card onClick={() => redirectToSpecificProductPage(item)}>
-                    <img src={item.url} alt={item.name} />
+                    <img src={item.url} alt="Processador" />
                     <span>{item.name.slice(0, 30)}</span>
-                    {/* <p>{formatValue(item.price)}</p> */}
                   </S.Card>
                 </S.SliderChild>
               ))}
-            </Slider>
-          }
-          <Button
-            width="300px"
-            style={{
-              fontSize: "1.2rem",
-              borderRadius: "10px",
-              color: "white",
-              height: "50px",
-              marginBottom: "20px",
-            }}
-            onClick={getToRegisterProducts}
-          >
-            Anunciar novo produto
-          </Button>
-        </>
-      )}
-    </S.Container>
+          </Carousel>
+        </div>
+      </S.Container>
+    </>
   );
 };
 
